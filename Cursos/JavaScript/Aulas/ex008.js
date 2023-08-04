17b4e5446ab50b8a245941df814ac9df283a03d7
Ivan Neto <ivansinetto@gmail.com>
2023-08-04 03:12:09 -0300

    Update folder
    Adicionando material de estudo ao repositório.

---
+++ Cursos/JavaScript/Aulas/ex008.js
@@ -0,0 +1,6 @@
+var val = 30
+console.log(`A velocidade do seu carro é ${val}Km/h`)
+if (val > 60) { // condição simples!
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
