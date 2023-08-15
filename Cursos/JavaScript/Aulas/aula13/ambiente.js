4ca17f70687a636390e70a1b4329b20d6bad304f
Ivan Neto <ivansinetto@gmail.com>
2023-08-15 08:26:24 -0300

    daily

---
+++ Cursos/JavaScript/Aulas/aula13/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (let c = 1; c <= 5; c = c + 1) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
---
+++ Cursos/JavaScript/Aulas/aula14ex/ex17/script.js
@@ -0,0 +1,3 @@
+var num = document.getElementById('num')
+var numero = Number(num.value)
+var tabuada = ''
