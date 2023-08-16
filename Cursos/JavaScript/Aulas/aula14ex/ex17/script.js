77ab00447a7f80e93a4995ad8ffa8e03e59da3d0
Ivan Neto <ivansinetto@gmail.com>
2023-08-16 09:22:04 -0300

    Daily update

--- Cursos/JavaScript/Aulas/aula14ex/ex17/script.js
+++ Cursos/JavaScript/Aulas/aula14ex/ex17/script.js
@@ -1,3 +1,15 @@
-var num = document.getElementById('num')
-var numero = Number(num.value)
-var tabuada = ''
+function calcular() {
+    num = document.getElementById('txtn')
+    n = Number(num.value)
+    res = document.getElementById('res')
+
+    res.innerHTML = ''
+
+    if (n == 0) {
+        alert('[ERRO] você não digitou um número!')
+    } else {
+        for(let c = 1; c <= 10; c++) {
+            res.innerHTML += `${n} X ${c} = ${n*c} <br>`
+        }
+    }
+}
