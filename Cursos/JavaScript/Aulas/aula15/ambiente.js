0287d7fc65eedd46fab8a808e70b5439671cda7d
Ivan Neto <ivansinetto@gmail.com>
2023-08-16 11:34:51 -0300

    0.5 up

---
+++ Cursos/JavaScript/Aulas/aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3] //vetor
+num.sort() //ordem crescente
+num.push(1) //adicionei mais um valor dentro do vetor
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ Cursos/JavaScript/Aulas/aula15/vetornatela.js
@@ -0,0 +1,22 @@
+valores = [8, 1, 7, 4, 2, 9]
+valores.sort()
+for(let posicao in valores) {
+    console.log(`A posição ${posicao} tem o valor ${valores[posicao]}`)
+}
+
+/*
+for( let posicao = 0; posicao < valores.length; posicao++){
+    console.log(`A posição ${posicao} tem o valor ${valores[posicao]}`)
+}
+*/
+
+// console.log(valores)
+
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+*/
