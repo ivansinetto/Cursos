fbfcf614803daf815d3fdc318018a7da818209e5
Ivan Neto <ivansinetto@gmail.com>
2023-08-17 11:00:26 -0300

    funcoes_update

---
+++ Cursos/JavaScript/Aulas/aula15/funcao01.js
@@ -0,0 +1,12 @@
+function parimp(n) {
+    if (n%2==0) {
+        return 'Par!'
+    } else {
+        return 'Ìmpar!'
+    }
+}
+
+/*let res = parimp(5)
+console.log(res) */
+
+console.log(parimp(101))
